642ecbd3881caf8eb799c0e05e5899cd4cfcdd87
Noah Wisch <noah.wisch@gmail.com>
2017-03-14 16:25:17 -0400

    Fixed the app.js

--- js/app.js
+++ js/app.js
@@ -1,38 +1,37 @@
-const app = angular.module('BibliocacheApp', ['ui.router', 'ngMessages']).run(function ($rootScope, $state, $stateParams) {
-});
+const app = angular.module('EventsApp', ['ui.router']);
 
-/* Routes */
-const routes = require('./routes');
+// /* Routes */
+// const routes = require('./routes');
 
-app.config($stateProvider => {
-	for (let i = 0; i < routes.length; i++) {
-		$stateProvider.state(routes[i]);
-	}
-});
+// app.config($stateProvider => {
+// 	for (let i = 0; i < routes.length; i++) {
+// 		$stateProvider.state(routes[i]);
+// 	}
+// });
 
-/* Controllers */
-const controllers = [
-	require('./controllers/#'),	
-];
+// /* Controllers */
+// const controllers = [
+// 	require('./controllers/#'),	
+// ];
 
-for (let i = 0; i < controllers.length; i++) {
-	app.controller(controllers[i].name, controllers[i].func);
-}
+// for (let i = 0; i < controllers.length; i++) {
+// 	app.controller(controllers[i].name, controllers[i].func);
+// }
 
-/* Components */
-const components = [
-	require('./components/#'),
-];
+// /* Components */
+// const components = [
+// 	require('./components/#'),
+// ];
 
-for (let i = 0; i < components.length; i++) {
-	app.component(components[i].name, components[i].func);
-}
+// for (let i = 0; i < components.length; i++) {
+// 	app.component(components[i].name, components[i].func);
+// }
 
-/* Services */
-const services = [
-	require('./services/#'),
-];
+// /* Services */
+// const services = [
+// 	require('./services/#'),
+// ];
 
-for (let i = 0; i < services.length; i++) {
-	app.factory(services[i].name, services[i].func);
-}
+// for (let i = 0; i < services.length; i++) {
+// 	app.factory(services[i].name, services[i].func);
+// }
