5b29cdc4036b5024d332eea026146fb21d80ab1c
Noah Wisch <noah.wisch@gmail.com>
2017-03-14 16:08:33 -0400

    Added skeleton files

--- js/app.js
+++ js/app.js
@@ -1,4 +1,38 @@
-
-window.addEventListener('load', () => {
-    console.log('hi there');
+const app = angular.module('BibliocacheApp', ['ui.router', 'ngMessages']).run(function ($rootScope, $state, $stateParams) {
 });
+
+/* Routes */
+const routes = require('./routes');
+
+app.config($stateProvider => {
+	for (let i = 0; i < routes.length; i++) {
+		$stateProvider.state(routes[i]);
+	}
+});
+
+/* Controllers */
+const controllers = [
+	require('./controllers/#'),	
+];
+
+for (let i = 0; i < controllers.length; i++) {
+	app.controller(controllers[i].name, controllers[i].func);
+}
+
+/* Components */
+const components = [
+	require('./components/#'),
+];
+
+for (let i = 0; i < components.length; i++) {
+	app.component(components[i].name, components[i].func);
+}
+
+/* Services */
+const services = [
+	require('./services/#'),
+];
+
+for (let i = 0; i < services.length; i++) {
+	app.factory(services[i].name, services[i].func);
+}
